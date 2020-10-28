08201c5f91c66742f2cda1f0fa9d157991a40a48
junstyle <junstyle@users.noreply.github.com>
2020-10-28 16:24:11 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -2877,12 +2877,12 @@ update_v2ray() {
 	mkdir -p /tmp/v2ray
 
 	v2ray_tmp_file="/tmp/v2ray/v2ray.zip"
-	v2ray_latest_ver="$(curl -s "https://api.github.com/repos/v2ray/v2ray-core/releases/latest?r=$RANDOM" | grep 'tag_name' | cut -d\" -f4)"
+	v2ray_latest_ver="$(curl -s "https://api.github.com/repos/v2fly/v2ray-core/releases/latest?r=$RANDOM" | grep 'tag_name' | cut -d\" -f4)"
 	if [[ $v2ray_ver != $v2ray_latest_ver ]]; then
 		echo
 		echo -e " $green 咦...发现新版本耶....正在拼命更新.......$none"
 		echo
-		v2ray_download_link="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
+		v2ray_download_link="https://github.com/v2fly/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
 
 		if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 			echo -e "
